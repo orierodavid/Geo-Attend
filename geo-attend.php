39e3cc0d365c3e4d50608df6887b6b90f7ccb837
orierodavid <orierodavid@yahoo.com>
2026-09-11 21:36:06 +0100

    Bump Geo-Attend to 0.2.1 and run DB upgrades on plugin load

--- geo-attend.php
+++ geo-attend.php
@@ -3,7 +3,7 @@
  * Plugin Name: Geo-Attend
  * Plugin URI: https://github.com/orierodavid/Geo-Attend
  * Description: Configurable geofenced attendance for organizations, churches, schools and teams. Uses the WordPress database with no external database dependency.
- * Version: 0.2.0
+ * Version: 0.2.1
  * Requires at least: 6.5
  * Requires PHP: 7.4
  * Author: Deotech Web Technologies
@@ -16,7 +16,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'GEO_ATTEND_VERSION', '0.2.0' );
+define( 'GEO_ATTEND_VERSION', '0.2.1' );
 define( 'GEO_ATTEND_FILE', __FILE__ );
 define( 'GEO_ATTEND_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GEO_ATTEND_URL', plugin_dir_url( __FILE__ ) );
@@ -28,6 +28,7 @@ require_once GEO_ATTEND_DIR . 'includes/class-geo-attend-admin.php';
 register_activation_hook( __FILE__, array( 'Geo_Attend_DB', 'activate' ) );
 
 add_action( 'plugins_loaded', function() {
+    Geo_Attend_DB::maybe_upgrade();
     Geo_Attend_Public::init();
     if ( is_admin() ) {
         Geo_Attend_Admin::init();
