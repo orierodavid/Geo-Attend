8c06c31673c146f2f0c975083ee81fb5bd2cba99
orierodavid <orierodavid@yahoo.com>
2026-09-12 00:28:49 +0100

    Clean up staff portal data on uninstall

--- uninstall.php
+++ uninstall.php
@@ -3,9 +3,17 @@ if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) { exit; }
 
 global $wpdb;
 $prefix = $wpdb->prefix . 'geo_attend_';
-$tables = array( 'attendance', 'members', 'locations', 'departments' );
+$tables = array( 'tasks', 'projects', 'attendance', 'members', 'locations', 'departments' );
 foreach ( $tables as $table ) {
     $wpdb->query( "DROP TABLE IF EXISTS {$prefix}{$table}" );
 }
+
+$page_id = absint( get_option( 'geo_attend_staff_portal_page_id', 0 ) );
+if ( $page_id ) {
+    wp_delete_post( $page_id, true );
+}
+
+delete_option( 'geo_attend_staff_portal_page_id' );
 delete_option( 'geo_attend_settings' );
 delete_option( 'geo_attend_version' );
+remove_role( 'geo_staff' );
